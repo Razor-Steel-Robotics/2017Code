26e641541e843e1be53f3e8dd20d1f6988a20329
Soohan Kim <soohankimdhs@gmail.com>
2017-02-20 17:29:10 -0500

    Update Robot.java

--- ArcadeDriveZaxis/src/org/usfirst/frc/team5938/robot/Robot.java
+++ ArcadeDriveZaxis/src/org/usfirst/frc/team5938/robot/Robot.java
@@ -1,15 +1,18 @@
 package org.usfirst.frc.team5938.robot;
 
+import com.ctre.CANTalon;
 import edu.wpi.first.wpilibj.IterativeRobot;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.Joystick;
-import com.kauailabs.navx.frc.AHRS;
-import edu.wpi.first.wpilibj.SerialPort;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.cscore.UsbCamera;
+import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.VictorSP;
+
+import com.kauailabs.navx.frc.AHRS;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -18,17 +21,71 @@ import edu.wpi.first.wpilibj.VictorSP;
  * creating this project, you must also update the manifest file in the resource
  * directory.
  */
-
-public class Robot extends IterativeRobot implements PIDOutput{
+public class Robot extends IterativeRobot {
 	final String defaultAuto = "Default";
 	final String customAuto = "My Auto";
 	String autoSelected;
-	SendableChooser<String> chooser = new SendableChooser<>();
-	RobotDrive myRobot;
-	Joystick mainStick;
-	AHRS ahrs;
-	int autoLoopCounter;
+	SendableChooser<String> chooser;
 	
+	RobotDrive myRobot;
+	Joystick driveStick;
+	Joystick xbox;
+	
+	CANTalon shooter;
+	CANTalon intake;
+	CANTalon winch;
+	
+	UsbCamera cam;
+	UsbCamera cam1;
+	AHRS ahrs;
+	
+	boolean buttonPress;
+	int lowerAngle = 50;
+	int upperAngle = 75;
+	
+	public void onCamera() {
+		cam = CameraServer.getInstance().startAutomaticCapture(0);
+		cam.setResolution(160, 120);
+		cam.setFPS(20);
+		cam1 = CameraServer.getInstance().startAutomaticCapture(1);
+		cam1.setResolution(160, 120);
+		cam1.setFPS(20); 
+		
+	}
+	
+	public void ahrsInit() {
+		
+		 try {
+
+
+	          ahrs = new AHRS(SPI.Port.kMXP); 
+
+
+
+	      } catch (RuntimeException ex ) {
+
+
+
+	          DriverStation.reportError("Error instantiating navX-MXP:  " + ex.getMessage(), true);
+
+
+
+	      }
+		
+	
+	}
+	
+	
+	public void rotateBot() {
+		
+
+		if(lowerAngle <= ahrs.getYaw() && ahrs.getYaw() <= upperAngle){
+
+			myRobot.drive(0, 0);
+			buttonPress = false;
+
+		}
+	}
 	
 	/**
 	 * This function is run when the robot is first started up and should be
@@ -36,29 +93,40 @@ public class Robot extends IterativeRobot implements PIDOutput{
 	 */
 	@Override
 	public void robotInit() {
-		myRobot = new RobotDrive(1, 0);
-		mainStick = new Joystick(0);
-	
-		 try {
-
-	          /* Communicate w/navX-MXP via the MXP SPI Bus.                                     */
-	          /* Alternatively:  I2C.Port.kMXP, SerialPort.Port.kMXP or SerialPort.Port.kUSB     */
-	          /* See http://navx-mxp.kauailabs.com/guidance/selecting-an-interface/ for details. */
-	          ahrs = new AHRS(SPI.Port.kMXP); 
-		 
-		 } catch (RuntimeException ex ) {
-
-	          DriverStation.reportError("Error instantiating navX-MXP:  " + ex.getMessage(), true);
-			 
-		 }
 		
+		chooser = new SendableChooser<>();
+		chooser.addDefault("Default Auto", defaultAuto);
+		chooser.addObject("My Auto", customAuto);
+		
+		SmartDashboard.putData("Auto choices", chooser);
+		onCamera();
+		ahrsInit();
+		
+		myRobot = new RobotDrive(0, 1); // class that handles basic drive operations
+		
+		xbox = new Joystick(0);
+		driveStick = new Joystick(1); // set to ID 1 in DriverStation
+		
+		intake = new CANTalon(1); 
+		shooter = new CANTalon(2);
+		winch = new CANTalon (3);
 	}
-	
-	
+
+	/**
+	 * This autonomous (along with the chooser code above) shows how to select
+	 * between different autonomous modes using the dashboard. The sendable
+	 * chooser code works with the Java SmartDashboard. If you prefer the
+	 * LabVIEW Dashboard, remove all of the chooser code and uncomment the
+	 * getString line to get the auto name from the text box below the Gyro
+	 *
+	 * You can add additional auto modes by adding additional comparisons to the
+	 * switch structure below with additional strings. If using the
+	 * SendableChooser make sure to add them to the chooser code above as well.
+	 */
 	@Override
 	public void autonomousInit() {
-	
-		
+		autoSelected = chooser.getSelected();
+		System.out.println("Auto selected: " + autoSelected);
 	}
 
 	/**
@@ -66,8 +134,15 @@ public class Robot extends IterativeRobot implements PIDOutput{
 	 */
 	@Override
 	public void autonomousPeriodic() {
-		
-		
+		switch (autoSelected) {
+		case customAuto:
+			// Put custom auto code here
+			break;
+		case defaultAuto:
+		default:
+			// Put default auto code here
+			break;
+		}
 	}
 
 	/**
@@ -76,38 +151,99 @@ public class Robot extends IterativeRobot implements PIDOutput{
 	@Override
 	public void teleopPeriodic() {
 		
+		myRobot.setSafetyEnabled(true);
 		myRobot.setSensitivity(.5);
 		
 		while (isOperatorControl() && isEnabled()) {
-			myRobot.arcadeDrive(mainStick , 1, mainStick, 2, true);
+			buttonPress = true;
+			myRobot.arcadeDrive(driveStick , 1, driveStick, 2, true);
 			
-			Timer.delay(0.05); /* wait for one motor update time period (50Hz)     */
+			
+			if (xbox.getRawAxis(2) >= .25) {
+				
+				intake.set(1);
+			
+			} else {
+			
+				intake.set(0);
+			
 			}
+		
+			if (xbox.getRawAxis(3) >= .25) {
+			
+				shooter.set(1);
+			
+			} else {
+			
+				shooter.set(0);
+			
+			}
+			
+			if (xbox.getRawButton(1) == true) {
+				
+				winch.set(1);
+			
+			} else {
+			
+				winch.set(0);
+			
+			}
+			
+			if (driveStick.getRawButton(1)) {	
 
-	          /* Display 6-axis Processed Angle Data                                      */
-	          SmartDashboard.putBoolean(  "IMU_Connected",        ahrs.isConnected());
+				while(buttonPress) {
+
+					myRobot.arcadeDrive(.1, .5);
+					myRobot.arcadeDrive(.1, -.5);
+					rotateBot();
+
+				}
+			}
+		
+			if (driveStick.getRawButton(2)) {
+
+				myRobot.arcadeDrive(.1, .5);
+				myRobot.arcadeDrive(.1, -.5);
+
+			}
+			
+			
+			
+			Timer.delay(0.005); // wait for a motor update time
+		}
+		
+		 SmartDashboard.putBoolean(  "IMU_Connected",        ahrs.isConnected());
+
 	          SmartDashboard.putBoolean(  "IMU_IsCalibrating",    ahrs.isCalibrating());
+
 	          SmartDashboard.putNumber(   "IMU_Yaw",              ahrs.getYaw());
+
 		
-	          /* These functions are compatible w/the WPI Gyro Class, providing a simple  */
-	          /* path for upgrading from the Kit-of-Parts gyro to the navx-MXP            */
+
+
 	          SmartDashboard.putBoolean(  "IMU_IsMoving",         ahrs.isMoving());
+
 	          SmartDashboard.putBoolean(  "IMU_IsRotating",       ahrs.isRotating());
+
 		
-	          /* Omnimount Yaw Axis Information                                           */
-	          /* For more info, see http://navx-mxp.kauailabs.com/installation/omnimount  */
+
+	         
+
 	          AHRS.BoardYawAxis yaw_axis = ahrs.getBoardYawAxis();
+
 	          SmartDashboard.putString(   "YawAxisDirection",     yaw_axis.up ? "Up" : "Down" );
+
 	          SmartDashboard.putNumber(   "YawAxis",              yaw_axis.board_axis.getValue() );
 
+
 	}
-		
+
 	/**
 	 * This function is called periodically during test mode
 	 */
 	@Override
 	public void testPeriodic() {
 	}
-
 	
+
 }
